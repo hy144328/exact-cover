64dfa4886aa62c5496427e6c2de92e3277e478e3
Hans Yu <hans.yu@outlook.de>
2021-08-08 16:13:50 +0200

    Convert JSON to pd.DataFrame.

---
+++ cover/io.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+import json
+import os
+import pandas as pd
+
+def read_json(data):
+    rows = list(data.keys())
+    no_rows = len(rows)
+
+    cols = set()
+    cols = list(cols.union(*data.values()))
+    no_cols = len(cols)
+
+    df = pd.DataFrame(
+        0,
+        index=rows,
+        columns=cols,
+        dtype=int,
+    )
+    for row_it in wiki_data:
+        df.loc[row_it, wiki_data[row_it]] = 1
+
+    return df
+
+if __name__ == "__main__":
+    file_path = os.path.join(
+        os.path.dirname(__file__),
+        os.path.pardir,
+        "cover",
+        "wiki.json",
+    )
+
+    with open(file_path, 'r') as f:
+        wiki_data = json.load(f)
+    df = read_json(wiki_data)
+
+    print(df)
