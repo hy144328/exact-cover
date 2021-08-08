14daf0be3fc6b901bb7b3d0f01e75a6038923b7d
Hans Yu <hans.yu@outlook.de>
2021-08-09 00:04:18 +0200

    Restore columns.

--- algo/__init__.py
+++ algo/__init__.py
@@ -6,33 +6,44 @@ import pandas as pd
 class Table(pd.DataFrame):
     def __init__(self, df):
         super().__init__(df)
+        self.current_index = self.index
+        self.current_columns = self.columns
+        self.current = self.loc[self.current_index, self.current_columns]
 
     def choose_rows(self, col) -> tuple:
-        return tuple(row_it for row_it in self.index if self.at[row_it, col] == 1)
+        return tuple(row_it for row_it in self.current_index if self.at[row_it, col] == 1)
 
     def choose_cols(self, row) -> tuple:
-        return tuple(col_it for col_it in self.columns if self.at[row, col_it] == 1)
+        return tuple(col_it for col_it in self.current_columns if self.at[row, col_it] == 1)
 
-    def delete_rows(self, rows: tuple) -> "Table":
-        new_index = [e for e in self.index if e not in set(rows)]
-        return Table(self.loc[new_index, :])
+    def delete_rows(self, rows: tuple):
+        self.current_index = [row_it for row_it in self.current_index if row_it not in set(rows)]
+        self.current = self.loc[self.current_index, self.current_columns]
 
-    def delete_cols(self, cols: tuple) -> "Table":
-        new_columns = [e for e in self.columns if e not in set(cols)]
-        return Table(self.loc[:, new_columns])
+    def delete_cols(self, cols: tuple):
+        self.current_columns = [col_it for col_it in self.current_columns if col_it not in set(cols)]
+        self.current = self.loc[self.current_index, self.current_columns]
+
+    def restore_rows(self, rows: tuple):
+        self.current_index = [row_it for row_it in self.index if row_it in self.current_index or row_it in rows]
+        self.current = self.loc[self.current_index, self.current_columns]
+
+    def restore_cols(self, cols: tuple):
+        self.current_columns = [col_it for col_it in self.columns if col_it in self.current_columns or col_it in cols]
+        self.current = self.loc[self.current_index, self.current_columns]
 
 
 class AlgorithmX:
     @classmethod
     def solve(cls, A: Table, sols: list[tuple], res: tuple = None):
-        if len(A.columns) == 0:
+        if len(A.current_columns) == 0:
             sols.append(res)
             return
 
         if not res:
             res = tuple()
 
-        col = next(iter(A.columns))
+        col = next(iter(A.current_columns))
         for row_it in A.choose_rows(col):
             res_it = res + (row_it, )
             cols_removed = A.choose_cols(row_it)
@@ -41,8 +52,11 @@ class AlgorithmX:
             for col_it in cols_removed:
                 rows_removed |= set(A.choose_rows(col_it))
 
-            new_A = A
-            new_A = new_A.delete_rows(rows_removed)
-            new_A = new_A.delete_cols(cols_removed)
-            cls.solve(new_A, sols, res_it)
+            A.delete_rows(rows_removed)
+            A.delete_cols(cols_removed)
+
+            cls.solve(A, sols, res_it)
+
+            A.restore_cols(reversed(cols_removed))
+            A.restore_rows(reversed(rows_removed))
 
