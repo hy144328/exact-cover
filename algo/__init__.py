3ed65985f06e1412425e2593dd92dd6c48da74c9
Hans Yu <hans.yu@outlook.de>
2021-08-09 21:50:42 +0200

    Introduce generators.

--- algo/__init__.py
+++ algo/__init__.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import abc
+from collections.abc import Iterable
 import pandas as pd
 
 
@@ -10,27 +11,27 @@ class Cover:
         ...
 
     @abc.abstractmethod
-    def choose_rows(self, col) -> tuple:
+    def choose_rows(self, col) -> Iterable:
         ...
 
     @abc.abstractmethod
-    def choose_cols(self, row) -> tuple:
+    def choose_cols(self, row) -> Iterable:
         ...
 
     @abc.abstractmethod
-    def delete_rows(self, rows: tuple):
+    def delete_rows(self, rows: Iterable):
         ...
 
     @abc.abstractmethod
-    def delete_cols(self, cols: tuple):
+    def delete_cols(self, cols: Iterable):
         ...
 
     @abc.abstractmethod
-    def restore_rows(self, rows: tuple):
+    def restore_rows(self, rows: Iterable):
         ...
 
     @abc.abstractmethod
-    def restore_cols(self, cols: tuple):
+    def restore_cols(self, cols: Iterable):
         ...
 
 
@@ -45,32 +46,32 @@ class IncidenceMatrix(Cover, pd.DataFrame):
     def next_col(self):
         return self.current_columns[0]
 
-    def choose_rows(self, col) -> tuple:
-        return tuple(row_it for row_it in self.current_index if self.at[row_it, col] == 1)
+    def choose_rows(self, col) -> Iterable:
+        return (row_it for row_it in self.current_index if self.at[row_it, col] == 1)
 
-    def choose_cols(self, row) -> tuple:
-        return tuple(col_it for col_it in self.current_columns if self.at[row, col_it] == 1)
+    def choose_cols(self, row) -> Iterable:
+        return (col_it for col_it in self.current_columns if self.at[row, col_it] == 1)
 
-    def delete_rows(self, rows: tuple):
+    def delete_rows(self, rows: Iterable):
         self.current_index = [row_it for row_it in self.current_index if row_it not in set(rows)]
         self.current = self.loc[self.current_index, self.current_columns]
 
-    def delete_cols(self, cols: tuple):
+    def delete_cols(self, cols: Iterable):
         self.current_columns = [col_it for col_it in self.current_columns if col_it not in set(cols)]
         self.current = self.loc[self.current_index, self.current_columns]
 
-    def restore_rows(self, rows: tuple):
-        self.current_index = [row_it for row_it in self.index if row_it in self.current_index or row_it in rows]
+    def restore_rows(self, rows: Iterable):
+        self.current_index = [row_it for row_it in self.index if row_it in self.current_index or row_it in set(rows)]
         self.current = self.loc[self.current_index, self.current_columns]
 
-    def restore_cols(self, cols: tuple):
-        self.current_columns = [col_it for col_it in self.columns if col_it in self.current_columns or col_it in cols]
+    def restore_cols(self, cols: Iterable):
+        self.current_columns = [col_it for col_it in self.columns if col_it in self.current_columns or col_it in set(cols)]
         self.current = self.loc[self.current_index, self.current_columns]
 
 
 class AlgorithmX:
     @staticmethod
-    def solve(A: Cover, solutions: list[tuple] = None, res: tuple = None):
+    def solve(A: Cover, solutions: list[tuple] = None, res: tuple = None) -> list[tuple]:
         if solutions is None:
             solutions = []
 
@@ -85,7 +86,7 @@ class AlgorithmX:
 
         for row_it in A.choose_rows(col):
             res_it = res + (row_it, )
-            cols_removed = A.choose_cols(row_it)
+            cols_removed = set(A.choose_cols(row_it))
 
             rows_removed = set()
             for col_it in cols_removed:
