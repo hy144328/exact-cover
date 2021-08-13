067f4732c7587e5efd600af6f4014f047723c431
Hans Yu <hans.yu@outlook.de>
2021-08-14 01:05:27 +0200

    Fix hitting_set/ tests.

--- hitting_set/links.py
+++ hitting_set/links.py
@@ -8,11 +8,11 @@ class DancingLinks(cover_links.DancingLinks):
     def read_json(data: dict[object, list]) -> "DancingLinks":
         constraints = list(data.keys())
         choices = set()
-        choices = list(constraints.union(*data.values()))
+        choices = list(choices.union(*data.values()))
 
         res = DancingLinks(choices, constraints)
         for constraint_it in data:
             for choice_it in data[constraint_it]:
-                res.insert(Node(), choice_it, constraint_it)
+                res.insert(cover_links.Node(), choice_it, constraint_it)
 
         return res
--- hitting_set/test_wiki.py
+++ hitting_set/test_wiki.py
@@ -2,19 +2,21 @@
 
 import pytest
 
-from cover.test_wiki import Wiki
+from cover import test_wiki as cover_test_wiki
 from .incidence import IncidenceMatrix
 from .links import DancingLinks
+
+
+class Wiki(cover_test_wiki.Wiki):
+    @pytest.fixture
+    def solution(self) -> tuple:
+        return (1, 2, 5)
 
 
 class TestWikiIncidenceMatrix(Wiki):
     @pytest.fixture
     def cover(self, data: dict[object, list]) -> IncidenceMatrix:
         return IncidenceMatrix.read_json(data)
-
-    @pytest.fixture
-    def solution(self) -> tuple:
-        return (1, 2, 5)
 
 
 class TestWikiDancingLinks(Wiki):
