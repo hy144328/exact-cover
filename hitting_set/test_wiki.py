be9adfa30d516763776e1a3315ecde07b6790492
Hans Yu <hans.yu@outlook.de>
2021-08-17 00:03:34 +0200

    Test hitting set.

--- hitting_set/test_wiki.py
+++ hitting_set/test_wiki.py
@@ -7,19 +7,53 @@ from .incidence import IncidenceMatrix
 from .links import DancingLinks
 
 
-class Wiki(cover_test_wiki.Wiki):
-    @pytest.fixture
-    def solution(self) -> tuple:
-        return (1, 2, 5)
-
-
-class TestWikiIncidenceMatrix(Wiki):
+class WikiIncidenceMatrix(cover_test_wiki.Wiki):
     @pytest.fixture
     def cover(self, data: dict[object, list]) -> IncidenceMatrix:
         return IncidenceMatrix.read_json(data)
 
 
-class TestWikiDancingLinks(Wiki):
+class WikiDancingLinks(cover_test_wiki.Wiki):
     @pytest.fixture
     def cover(self, data: dict[object, list]) -> DancingLinks:
         return DancingLinks.read_json(data)
+
+
+class WikiAlgorithmX(cover_test_wiki.WikiAlgorithmX):
+    @pytest.fixture
+    def solution(self) -> tuple:
+        return (1, 2, 5)
+
+
+class WikiConstraintProgramming(cover_test_wiki.WikiConstraintProgramming):
+    @pytest.fixture
+    def solution(self) -> tuple:
+        return ("1", "2", "5")
+
+
+class TestWikiAlgorithmXIncidenceMatrix(
+    WikiAlgorithmX,
+    WikiIncidenceMatrix,
+):
+    ...
+
+
+class TestWikiAlgorithmXDancingLinks(
+    WikiAlgorithmX,
+    WikiDancingLinks,
+):
+    ...
+
+
+class TestWikiConstraintProgrammingIncidenceMatrix(
+    WikiConstraintProgramming,
+    WikiIncidenceMatrix,
+):
+    ...
+
+
+class TestWikiConstraintProgrammingDancingLinks(
+    WikiConstraintProgramming,
+    WikiDancingLinks,
+):
+    ...
