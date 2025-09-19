dc566f8ba317a1e0e34e06e42e884bd413eb2659
Hans Yu <hans.yu@outlook.de>
2025-09-19 14:30:56 +0200

    Fix coverage.

--- exact_cover/cover/base.py
+++ exact_cover/cover/base.py
@@ -3,39 +3,39 @@ import abc
 class Cover[ChoiceT, ConstraintT](abc.ABC):
     @property
     @abc.abstractmethod
-    def choices(self) -> list[ChoiceT]:
+    def choices(self) -> list[ChoiceT]: # pragma: no cover
         raise NotImplementedError()
 
     @property
     @abc.abstractmethod
-    def constraints(self) -> list[ConstraintT]:
+    def constraints(self) -> list[ConstraintT]: # pragma: no cover
         raise NotImplementedError()
 
     @abc.abstractmethod
-    def get_choices(self, constraint: ConstraintT) -> list[ChoiceT]:
+    def get_choices(self, constraint: ConstraintT) -> list[ChoiceT]:    # pragma: no cover
         raise NotImplementedError()
 
     @abc.abstractmethod
-    def get_constraints(self, choice: ChoiceT) -> list[ConstraintT]:
+    def get_constraints(self, choice: ChoiceT) -> list[ConstraintT]:    # pragma: no cover
         raise NotImplementedError()
 
 class MutableCover[ChoiceT, ConstraintT](Cover[ChoiceT, ConstraintT]):
     @abc.abstractmethod
-    def drop_choice(self, choice: ChoiceT):
+    def drop_choice(self, choice: ChoiceT): # pragma: no cover
         raise NotImplementedError()
 
     @abc.abstractmethod
-    def drop_constraint(self, constraint: ConstraintT):
+    def drop_constraint(self, constraint: ConstraintT): # pragma: no cover
         raise NotImplementedError()
 
     @abc.abstractmethod
-    def restore_choice(self, choice: ChoiceT):
+    def restore_choice(self, choice: ChoiceT):  # pragma: no cover
         raise NotImplementedError()
 
     @abc.abstractmethod
-    def restore_constraint(self, constraint: ConstraintT):
+    def restore_constraint(self, constraint: ConstraintT):  # pragma: no cover
         raise NotImplementedError()
 
     @abc.abstractmethod
-    def next_constraint(self) -> ConstraintT:
+    def next_constraint(self) -> ConstraintT:   # pragma: no cover
         raise NotImplementedError()
