ceb4130d5b8e6a2267d3ba25556d4b240f9d4c9d
Hans Yu <hans.yu@outlook.de>
2025-09-22 15:53:31 +0200

    Test template.

--- tests/test_cv.py
+++ tests/test_cv.py
@@ -1,3 +1,4 @@
+import abc
 import cv2 as cv
 import numpy as np
 import numpy.typing as npt
@@ -32,55 +33,102 @@ def test_normalize_contour(detector: exact_cover.sudoku.cv.SudokuDetector):
 
     assert np.array_equal(res, sol)
 
-@pytest.fixture
-def img() -> npt.NDArray[np.uint8]:
-    res = cv.imread(
-        "assets/Screenshot from 2022-01-02 21-00-15.png",
-        cv.IMREAD_GRAYSCALE,
-    )
-    return res
+class TestCV(abc.ABC):
+    @pytest.fixture
+    @abc.abstractmethod
+    def img(self) -> npt.NDArray[np.uint8]:
+        raise NotImplementedError()
 
-@pytest.fixture
-def sol() -> list[list[int | None]]:
-    res: list[list[int | None]] = [
-        [
-            None
+    @pytest.fixture
+    @abc.abstractmethod
+    def sol(self) -> list[list[int | None]]:
+        raise NotImplementedError()
+
+    def test(
+        self,
+        img: npt.NDArray[np.uint8],
+        sol: list[list[int | None]],
+        detector: exact_cover.sudoku.cv.SudokuDetector,
+    ):
+        img = detector.apply_blur(img)
+        img = detector.apply_threshold(img)
+        img = next(detector.extract_board(img))
+
+        squares = detector.extract_squares(img)
+
+        for i in range(9):
+            for j in range(9):
+                img_it = squares[i][j]
+                res_it, conf_it = exact_cover.sudoku.ocr.parse_digit(img_it)
+
+                assert res_it == sol[i][j]
+                if conf_it is not None:
+                    assert conf_it >= 0.6
+
+class TestScreenshot1(TestCV):
+    @pytest.fixture
+    def img(self) -> npt.NDArray[np.uint8]:
+        res = cv.imread(
+            "assets/Screenshot from 2022-01-02 21-00-15.png",
+            cv.IMREAD_GRAYSCALE,
+        )
+        return res
+
+    @pytest.fixture
+    def sol(self) -> list[list[int | None]]:
+        res: list[list[int | None]] = [
+            [
+                None
+                for _ in range(9)
+            ]
             for _ in range(9)
         ]
-        for _ in range(9)
-    ]
 
-    with open("assets/Screenshot from 2022-01-02 21-00-15.csv") as f:
-        data = [
-            line_it.split(",")
-            for line_it in f
+        with open("assets/Screenshot from 2022-01-02 21-00-15.csv") as f:
+            data = [
+                line_it.split(",")
+                for line_it in f
+            ]
+
+        for row_it in range(9):
+            for col_it in range(9):
+                try:
+                    res[row_it][col_it] = int(data[row_it][col_it])
+                except ValueError:
+                    pass
+
+        return res
+
+class TestScreenshot2(TestCV):
+    @pytest.fixture
+    def img(self) -> npt.NDArray[np.uint8]:
+        res = cv.imread(
+            "assets/Screenshot from 2022-01-06 13-35-44.png",
+            cv.IMREAD_GRAYSCALE,
+        )
+        return res
+
+    @pytest.fixture
+    def sol(self) -> list[list[int | None]]:
+        res: list[list[int | None]] = [
+            [
+                None
+                for _ in range(9)
+            ]
+            for _ in range(9)
         ]
 
-    for row_it in range(9):
-        for col_it in range(9):
-            try:
-                res[row_it][col_it] = int(data[row_it][col_it])
-            except ValueError:
-                pass
+        with open("assets/Screenshot from 2022-01-06 13-35-44.csv") as f:
+            data = [
+                line_it.split(",")
+                for line_it in f
+            ]
 
-    return res
+        for row_it in range(9):
+            for col_it in range(9):
+                try:
+                    res[row_it][col_it] = int(data[row_it][col_it])
+                except ValueError:
+                    pass
 
-def test(
-    img: npt.NDArray[np.uint8],
-    sol: list[list[int | None]],
-    detector: exact_cover.sudoku.cv.SudokuDetector,
-):
-    img = detector.apply_blur(img)
-    img = detector.apply_threshold(img)
-    img = next(detector.extract_board(img))
-
-    squares = detector.extract_squares(img)
-
-    for i in range(9):
-        for j in range(9):
-            img_it = squares[i][j]
-            res_it, conf_it = exact_cover.sudoku.ocr.parse_digit(img_it)
-
-            assert (res_it is not None) == (sol[i][j] is not None)
-            if conf_it is not None:
-                assert conf_it > 0.9
+        return res
